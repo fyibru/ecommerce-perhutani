9d3c81b78038ad873d45f437ee91739bfbb63ee4
yan <yansarprajtr@gmail.com>
2025-05-08 09:45:37 +0700

    add redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // penting!
+  images: {
+    unoptimized: true, // karena next/image tidak didukung saat export
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
